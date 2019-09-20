f6c0a0a0e915fdcda0e7e0db8b53ccd88270027b
Alex Somoza <ASomoza041@west-mec.org>
2019-09-19 22:30:46 -0700

    Project Complete
    Finished all videos.

--- learn_es6/main.js
+++ learn_es6/main.js
@@ -1,5 +1,8 @@
 "use strict"
 
+//VARIABLES
+
+
 // var a = "test1";
 // let b = "test2"
 
@@ -35,6 +38,336 @@ const colors = [];
 colors.push("red");
 colors.push("blue");
 
-colors = "green";
+//colors = "green";
 
-console.log(colors);
+//console.log(colors);
+
+
+//CLASSES
+
+class User {
+    constructor(username, email, password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    static countUsers() {
+        console.log("There are 50 users.")
+    }
+
+    register() {
+        console.log(this.username + " is now registered.");
+    }
+}
+
+//let bob = new User("bob", "bob @email.com", "12345");
+
+//bob.register();
+
+//User.countUsers();
+
+class Member extends User {
+    constructor(username, email, password, memberPackage) {
+        super(username, email, password);
+        this.package = memberPackage;
+    }
+
+    getPackage() {
+        console.log(this.username + " is subscriced to the " + this.package + " package.");
+    }
+}
+
+let mike = new Member("mike", "mike@email.com", "12345", "standard");
+
+//mike.getPackage();
+
+mike.register();
+
+
+
+
+//TEMPERATE LITERALS
+
+let name = "John"
+
+function makeUppercase(word) {
+    return word.toUpperCase();
+}
+
+let template = `<h1>${makeUppercase("Hello")}, ${name}</h1>
+    <p>This is a simple template in JavaScript</p>`;
+
+document.getElementById("template").innerHTML = template;
+
+//NEW STRING AND NUMBER METHODS
+
+let theString = "Hello, my name is Brad and I love JavaScript."
+
+//startsWith()
+
+// console.log(theString.startsWith("Hello"));
+
+//endsWith()
+
+// console.log(theString.endsWith("JavaScript."));
+
+//includes()
+
+// console.log(theString.includes("JavaScript."));
+
+
+//Hex
+// console.log(0xFF);
+
+//Binary
+// console.log(0b101011);
+
+//Octol
+// console.log(0o543);
+
+
+// console.log(Number.isFinite(3));
+
+// console.log(Number.isFinite(Infinity));
+
+
+// console.log(Number.isNaN("string"));
+
+// console.log(Number.isNaN(NaN));
+
+// console.log(Number.isInteger(NaN));
+
+// console.log(Number.isInteger(2));
+
+
+//DEFAULT PARAMS AND SPREAD OPERATOR
+
+function greet($greeting = "Hello world") {
+    console.log($greeting);
+}
+
+greet();
+
+
+
+let args1 = [1, 2, 3];
+let args2 = [4, 5, 6];
+
+function test() {
+    console.log(args1 + "," + args2);
+}
+
+//test.apply(null, args);
+
+test(...args1, ...args2);
+
+
+
+//SET, MAP, WEAKSET, WEAKMAP
+
+
+//SET
+// let myArray = [11, 22, 33, 44];
+// let mySet = new Set(myArray);
+
+// mySet.add("100");
+// mySet.add({
+//     a: 1,
+//     b: 2
+// });
+// mySet.delete(22);
+
+// mySet.add("100");
+// mySet.add("200");
+
+// //console.log(mySet.size);
+
+// mySet.forEach(function (val) {
+//     console.log(val);
+// });
+
+
+//MAP
+
+// let myMap = new Map([
+//     ["a1", "Hello"],
+//     ["b2", "Goodbye"]
+// ]);
+
+// myMap.set("c3", "foo");
+// myMap.delete("a1");
+
+// console.log(myMap.size);
+
+
+//WEAKSET
+
+// let carWeakSet = new WeakSet();
+
+// let car1 = {
+//     make: 'Honda',
+//     model: "civic"
+// }
+
+// carWeakSet.add(car1);
+
+// let car2 = {
+//     make: 'Toyota',
+//     model: "Camry"
+// }
+
+// carWeakSet.add(car2);
+
+// carWeakSet.delete(car1);
+// console.log(carWeakSet);
+
+
+//WEAKMAP
+
+// let carWeakMap = new WeakMap();
+
+// let key1 = {
+//     id: 1
+// }
+
+// let car1 = {
+//     make: 'Honda',
+//     model: "civic"
+// }
+
+// let key2 = {
+//     id: 2
+// }
+
+// let car2 = {
+//     make: 'Toyota',
+//     model: "camry"
+// }
+
+// carWeakMap.set(key1, car1);
+// carWeakMap.set(key2, car2);
+
+// carWeakMap.delete(key1);
+
+// console.log(carWeakMap);
+
+
+//ARROW FUNCTIONS
+
+// function Prefixer(prefix) {
+//     this.prefix = prefix;
+// }
+
+// Prefixer.prototype.prefixArray = function (arr) {
+//     return arr.map((x) => {
+//         console.log(this.prefix + x);
+//     });
+// }
+
+// let pre = new Prefixer("Hello ");
+// pre.prefixArray(["Brad", "Jeff"]);
+
+
+
+
+
+// let add = function (a, b) {
+//     let sum = a + b;
+//     console.log(sum);
+//     return false;
+// }
+
+// add(2, 2);
+
+
+let add = (a, b) => {
+    let sum = a + b;
+    console.log(sum);
+    return false;
+}
+
+add(2, 2);
+
+
+
+//PROMISES
+
+//immediately resolved promise
+
+//var myPromise = Promise.resolve("foo");
+
+//myPromise.then((res) => console.log(res));
+
+// var myPromise = new Promise(function (resolve, reject) {
+//     setTimeout(() => resolve(4), 2000);
+// });
+
+// myPromise.then((res) => {
+//     res += 3;
+//     console.log(res);
+// });
+
+
+// function getData(method, url) {
+//     return new Promise(function (resolve, reject) {
+//         var xhr = new XMLHttpRequest();
+//         xhr.open(method, url);
+//         xhr.onload = function () {
+//             if (this.status === 200 && this.status < 300) {
+//                 resolve(xhr.response);
+//             } else {
+//                 reject({
+//                     status: this.status,
+//                     statusText: xhr.statusText
+//                 });
+//             }
+//         };
+//         xhr.onerror = function () {
+//             reject({
+//                 status: this.status,
+//                 statusText: xhr.statusText
+//             });
+//         };
+//         xhr.send();
+//     });
+// }
+
+// getData("GET", "http://jsonplaceholder.typicode.com/todos").then(function (data) {
+//     let todos = JSON.parse(data);
+//     let output = "";
+//     for (let todo of todos) {
+//         output += `
+//         <li>
+//         <h3>${todo.title}<h3>
+//         <p>Completed: ${todo.completed}</p>
+//         </li>
+//         `;
+//     }
+
+//     document.getElementById("template").innerHTML = output;
+// }).catch(function (err) {
+//     console.log(err);
+// });
+
+
+
+//GENERATORS 
+
+function* g1() {
+    console.log("Hello");
+    yield "Yield 1 Ran..";
+    console.log("world");
+    yield "Yield 2 ran..";
+    return "Returned";
+}
+
+var g = g1();
+
+// console.log(g.next());
+// console.log(g.next());
+// console.log(g.next());
+
+for (let val of g) {
+    console.log(val);
+}
